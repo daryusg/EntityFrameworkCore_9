32afa9dbe5adaa8540bd4f24cbdf3451ea05b1d8
kev <kev@daryus.com>
2025-05-10 13:36:57 +0100

    cip...33

--- EntityFrameworkCore.Data/FootballLeagueDbContext.cs
+++ EntityFrameworkCore.Data/FootballLeagueDbContext.cs
@@ -1,38 +1,43 @@
 ﻿using EntityFrameworkCore.Domain;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Emit;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace EntityFrameworkCore.Data;
 
 public class FootballLeagueDbContext : DbContext
 {
-  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-  {
-    //base.OnConfiguring(optionsBuilder); //cip...16. default not needed.
-    //using sqlserver
-    //optionsBuilder.UseSqlServer("Data Source=localhost,1448;Initial Catalog=FootballLeague_EFCore;Encrypt=False;user id=sa;password=Str0ngPa$$w0rd;"); //to be tested
-    optionsBuilder.UseSqlite($"Data Source=FootballLeague_EFCore.db");
-  }
+    public FootballLeagueDbContext()
+    {
+        //cip...29. set the path to the database file in the local application data folder.
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = Path.Combine(path, "FootballLeague_EFCore.db"); //<ctrl>. -> "Generate property DbPath"
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //base.OnConfiguring(optionsBuilder); //cip...16. default not needed.
+        //using sqlserver
+        //optionsBuilder.UseSqlServer("Data Source=localhost,1448;Initial Catalog=FootballLeague_EFCore;Encrypt=False;user id=sa;password=Str0ngPa$$w0rd;"); //to be tested
+        //optionsBuilder.UseSqlite($"Data Source=FootballLeague_EFCore.db");
+        optionsBuilder.UseSqlite($"Data Source={DbPath}")
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableSensitiveDataLogging()
+            .EnableDetailedErrors(); //run update-database (pmc) to create the database file (in the common location).
+    }
 
-  protected override void OnModelCreating(ModelBuilder modelBuilder)
-  {
-    //base.OnModelCreating(modelBuilder);
-    modelBuilder.Entity<Team>().HasData(
-      new Team { Id = 1, Name = "Tivoli Gardens FC", CreatedDate = new DateTime(2025, 5, 9, 18, 0, 0) }, //hard-coding due to migration errors. DateTimeOffset.UtcNow.DateTime
-      new Team { Id = 2, Name = "Waterhouse FC", CreatedDate = new DateTime(2025, 5, 9, 18, 0, 0) },
-      new Team { Id = 3, Name = "Humble Lions FC", CreatedDate = new DateTime(2025, 5, 9, 18, 0, 0) }
-    );
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Team>().HasData(
+          new Team { Id = 1, Name = "Tivoli Gardens FC", CreatedDate = new DateTime(2025, 5, 9, 18, 0, 0) }, //hard-coding due to migration errors. DateTimeOffset.UtcNow.DateTime
+          new Team { Id = 2, Name = "Waterhouse FC", CreatedDate = new DateTime(2025, 5, 9, 18, 0, 0) },
+          new Team { Id = 3, Name = "Humble Lions FC", CreatedDate = new DateTime(2025, 5, 9, 18, 0, 0) }
+        );
+    }
 
-  }
-
-  public DbSet<Team> Teams { get; set; }
-  public DbSet<Coach> Coaches { get; set; }
-  //public DbSet<League> Leagues { get; set; }
-  //public DbSet<Match> Matches { get; set; }
+    public DbSet<Team> Teams { get; set; }
+    public DbSet<Coach> Coaches { get; set; }
+    public string DbPath { get; private set; }
+    //public DbSet<League> Leagues { get; set; }
+    //public DbSet<Match> Matches { get; set; }
 }
